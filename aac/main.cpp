ed18304c42f1e5c24008f8d08102a84d63115764
chan <1355425625@qq.com>
2018-12-10 21:40:09 +0800

    add note

--- aac/main.cpp
+++ aac/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+FILE *aac = fopen("/Users/chan/Documents/github/learn-video/media/nocturne.aac", "rb");
+
 int main(int argc, char* argv[])
 {
     return 0;
---
+++ note/jvm.md
@@ -0,0 +1,3 @@
+## 为什么java中的cclass只能有一个super class
+
+> .class文件中只有一个属性可以指向super class
