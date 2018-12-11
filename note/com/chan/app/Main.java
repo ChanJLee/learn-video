39baf6f68201073173e93a8e50a4efce847352a2
chan <1355425625@qq.com>
2018-12-11 23:13:45 +0800

    add demo class

---
+++ note/com/chan/app/Main.java
@@ -0,0 +1,27 @@
+package com.chan.app;
+
+public class Main {
+    public static final String TAG = "Main";
+    public static final int ID = 0xf0c0;
+
+    private int mId = 10;
+    private String mName = "jack";
+
+    public static void main(String args[]) {
+        System.out.println("hello world");
+    }
+
+    private void foo() {
+        for (int i = 0; i < 10; ++i) {
+            System.out.println("hello");
+        }
+    }
+
+    private void hugo() {
+        try {
+            System.out.println("hugo");
+        } catch(Exception e) {
+            System.out.println("exception");
+        }
+    }
+}
