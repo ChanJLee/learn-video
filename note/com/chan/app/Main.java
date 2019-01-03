06b285f31d0a66140c3e3da4628e031c5c25cbc0
chan <1355425625@qq.com>
2019-01-03 22:42:59 +0800

    add feature

--- note/com/chan/app/Main.java
+++ note/com/chan/app/Main.java
@@ -1,6 +1,9 @@
 package com.chan.app;
 
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 public class Main {
     public static final String TAG = "Main";
@@ -9,6 +12,57 @@ public class Main {
     private int mId = 10;
     private String mName = "jack";
 
+    private static void testFeature() {
+        FutureTask<Integer> task = new FutureTask<Integer>(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                try {
+                    for (int i = 0; i < 10; ++i) {
+                        System.out.println("times " + i);
+                        Thread.sleep(1000);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                return 12;
+            }
+        });
+
+        new Thread(task).start();
+        try {
+            System.out.println("feature result: " + task.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void testInterrupt() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while (true) {
+                    try {
+                        Thread.sleep(1000);
+                        System.out.println("fuck");
+                    } catch (InterruptedException e) {
+                        System.out.println("InterruptedException");
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        });
+        thread.start();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread.interrupt();
+    }
     public static void main(String args[]) {
         System.out.println("hello world");
         foo();
